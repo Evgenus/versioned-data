a35db92e4e9f3bf478665bb411944743df30577a
Evgenus <chernyshov.eugene@gmail.com>
2016-12-30 01:36:32 +0200

    registered placeholders

--- versioned/placeholders.py
+++ versioned/placeholders.py
@@ -1,7 +1,16 @@
+from .types import get_registry
+
 class List(list):
     def __hash__(self):
         return id(self)
 
+
+get_registry().register(list, List)
+
+
 class Dict(dict):
     def __hash__(self):
         return id(self)
+
+
+get_registry().register(dict, Dict)
